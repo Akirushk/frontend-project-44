1c163dd08480b6ecec1b85f9783daec40330f7e9
Akirushk <akirushk@mail.ru>
2024-05-10 15:00:44 +0300

    rename src/even-or-odd.js src/random-num.js

--- src/even-or-odd.js
+++
@@ -1,3 +0,0 @@
-const evenNumDeterminant = (num) => (num % 2 === 0 ? 'yes' : 'no');
-
-export default evenNumDeterminant;
--- src/random-num.js
+++
@@ -1,5 +0,0 @@
-const getRandomNum = (max, min) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-export default getRandomNum;
