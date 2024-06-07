f9838c054a6477d502f8f3db81739a92221a9a98
Akirushk <akirushk@mail.ru>
2024-06-07 23:14:08 +0300

    add parser function, fix function nesting

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -8,22 +8,37 @@ const getRandomOperator = () => {
   return operators[random];
 };
 
+const getQuestion = () => {
+  const min = 0;
+  const max = 100;
+
+  return `${getRandomNum(min, max)} ${getRandomOperator()} ${getRandomNum(min, max)}`;
+};
+
+const doVerification = (question) => {
+  const arr = question.split(' ');
+  const operator = arr[1];
+  const num1 = arr[0];
+  const num2 = arr[2];
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = Number(num1) + Number(num2);
+      break;
+    case '-':
+      result = Number(num1) - Number(num2);
+      break;
+    case '*':
+      result = Number(num1) * Number(num2);
+      break;
+    // no default
+  }
+  return String(result);
+};
+
 const play = () => {
   const description = 'What is the result of the expression?';
-
-  const getQuestion = () => {
-    const min = 0;
-    const max = 100;
-
-    return `${getRandomNum(min, max)} ${getRandomOperator()} ${getRandomNum(min, max)}`;
-  };
-
-  const doVerification = (question) => {
-    const toNum = eval;
-    const result = toNum(question);
-
-    return String(result);
-  };
 
   run(description, getQuestion, doVerification);
 };
