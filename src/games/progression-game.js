1c0dc9891fdcc7158b49a7a16f1f2cea5a77ef79
Akirushk <akirushk@mail.ru>
2024-06-26 08:03:36 +0300

    update doVerification() in progression-game.js

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -5,11 +5,11 @@ const description = 'What number is missing in the progression?';
 
 const createProgression = () => {
   const arr = [];
-  const num = getRandomNum(1, 5);
+  const step = getRandomNum(1, 5);
   const arrLength = getRandomNum(5, 10);
   let result = getRandomNum(0, 50);
 
-  for (let i = 0, j = 0; i < arrLength; i += 1, j = num) {
+  for (let i = 0, j = 0; i < arrLength; i += 1, j = step) {
     result += j;
     arr.push(result);
   }
@@ -17,15 +17,12 @@ const createProgression = () => {
   return arr;
 };
 
-let answer;
-
 const getQuestion = () => {
   const arr = createProgression();
   const random = getRandomNum(0, arr.length);
 
   for (let i = 0; i < arr.length; i += 1) {
     if (i === random) {
-      answer = arr[i];
       arr[i] = '..';
     }
   }
@@ -33,7 +30,28 @@ const getQuestion = () => {
   return arr.join(' ');
 };
 
-const doVerification = () => String(answer);
+const doVerification = (question) => {
+  const progression = question.split(' ');
+  let step;
+  let result;
+  let i = 0;
+
+  do {
+    step = Number(progression[i + 1]) - Number(progression[i]);
+    i += 1;
+  } while (Number.isNaN(step));
+
+  for (let j = 0; j < progression.length; j += 1) {
+    if (progression[j] === '..') {
+      if (j === (progression.length - 1)) {
+        result = Number(progression[j - 1]) + step;
+      } else {
+        result = Number(progression[j + 1]) - step;
+      }
+    }
+  }
+  return String(result);
+};
 
 const play = () => {
   run(description, getQuestion, doVerification);
