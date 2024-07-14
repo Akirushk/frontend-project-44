fac27835a50628b34e5bb4a8d3e51dcb299e0193
Akirushk <akirushk@mail.ru>
2024-07-14 21:20:48 +0300

    swaped arguments

--- src/get-random-num.js
+++ src/get-random-num.js
@@ -1,3 +1,3 @@
-const getRandomNum = (max, min) => Math.floor(Math.random() * (max - min + 1) + min);
+const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 export default getRandomNum;
