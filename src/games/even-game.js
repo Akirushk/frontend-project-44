2c498386afffee93275dd52751206b0c2903c223
Akirushk <akirushk@mail.ru>
2024-08-08 14:43:07 +0300

    rename description into DESCRIPTION, isEvenNum() into isEven()

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,16 +1,16 @@
 import getRandomNum from '../get-random-num.js';
 import run from '../index.js';
 
-const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEvenNum = (num) => num % 2 === 0;
+const isEven = (num) => num % 2 === 0;
 
 const getQuestion = () => getRandomNum(0, 100);
 
-const getAnswer = (question) => (isEvenNum(question) ? 'yes' : 'no');
+const getAnswer = (question) => (isEven(question) ? 'yes' : 'no');
 
 const play = () => {
-  run(description, getQuestion, getAnswer);
+  run(DESCRIPTION, getQuestion, getAnswer);
 };
 
 export default play;
